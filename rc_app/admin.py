e91ed00641100e986ebe68e04c14ff9d12e55627
ParasY1724 <parasyerunkar174@gmail.com>
2024-01-15 17:07:17 +0530

    __

--- rc_app/admin.py
+++ rc_app/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import UserTable,Question,TeamTable
+
 # Register your models here.
-
-admin.site.register(UserTable)
-admin.site.register(Question)
-admin.site.register(TeamTable)
-
