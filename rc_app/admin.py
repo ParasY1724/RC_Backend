4ecdf1ff99635e4201683198cd1498feb4f8a0d7
ParasY1724 <135828004+ParasY1724@users.noreply.github.com>
2024-01-19 21:57:18 +0530

    Update admin.py

--- rc_app/admin.py
+++ rc_app/admin.py
@@ -1,3 +1,8 @@
+# admin.py
 from django.contrib import admin
+from .models import Question, Team, Progress
 
-# Register your models here.
+# Register your models with admin.site.register
+admin.site.register(Question)
+admin.site.register(Team)
+admin.site.register(Progress)
